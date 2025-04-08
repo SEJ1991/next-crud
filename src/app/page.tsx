c5e18a0b3cb3b163f53cd59f15da8296a24c0439
SEJ <jink6111@gmail.com>
2025-04-08 17:19:22 +0900

    feat: [NC_publish-HomePage-component-14] published HomePage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
+import { PageFrame } from '@/shared/components/atoms/PageFrame';
+import Link from 'next/link';
+
 export default function Home() {
-  return <div>123123</div>;
+  return (
+    <PageFrame className='flex-center flex-col gap-4'>
+      <div className='flex-center flex-col'>
+        <h1 className='text-4xl text-accent-primary'>Next.js CRUD reference website.</h1>
+        <h2 className='text-2xl'>Domain structure.</h2>
+      </div>
+      <Link
+        href='https://github.com/SEJ1991/next-crud'
+        target='_blank'
+        rel='noopener noreferrer'
+        className='animate-bounce transition-opacity duration-300 hover:opacity-75'
+      >
+        Github ↗️
+      </Link>
+    </PageFrame>
+  );
 }
