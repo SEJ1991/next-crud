bdcfe56fa2da7d1d4f4914a7d2042e38dde1b620
SEJ <jink6111@gmail.com>
2025-04-09 16:46:00 +0900

    refactor: [NV_refactor-ContentsLayout-style-24] refactor ContentsLayout style

--- src/app/(contents)/layout.tsx
+++ src/app/(contents)/layout.tsx
@@ -5,8 +5,10 @@ interface Props {
 }
 export default function ContentsLayout({ children }: Readonly<Props>) {
   return (
-    <div className='grid grid-cols-[18.75rem_repeat(auto-fit,minmax(0,1fr))]'>
-      <SideBar>
+    <div className='grid grid-cols-[15.625rem_repeat(auto-fit,_minmax(0,1fr))]'>
+      <SideBar className='flex flex-col gap-4 max-w-dvh px-page-frame-x pb-page-frame-y overflow-y-auto'>
+        <h2 className='text-2xl text-accent-primary'>Contents</h2>
+        <hr className='text-border-primary' />
         <ContentsNav />
       </SideBar>
       {children}
