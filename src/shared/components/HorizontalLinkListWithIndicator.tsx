e7b07bdee7c0d8f489cb4ada5d52c4d8a50d4c38
SEJ <jink6111@gmail.com>
2025-04-12 01:18:29 +0900

    refactor: [NC_publish-HorizontalLinkListWithIndicator-component-37] remove Indicator component in this componet

--- src/shared/components/HorizontalLinkListWithIndicator.tsx
+++ src/shared/components/HorizontalLinkListWithIndicator.tsx
@@ -1,15 +1,21 @@
 'use client';
-import { NextLink, Indicator } from '@/shared';
-import { HTMLMotionProps } from 'framer-motion';
+import { NextLink } from '@/shared';
+import { HTMLMotionProps, motion } from 'framer-motion';
 import { usePathname } from 'next/navigation';
 import { ComponentPropsWithoutRef, useEffect, useState } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 interface Props extends ComponentPropsWithoutRef<'ul'> {
   links?: { href: string; label: React.ReactNode }[];
+  layoutId: string;
   indicatorProps?: HTMLMotionProps<'div'>;
 }
-export function HorizontalLinkListWithIndicator({ links = [], indicatorProps, ...props }: Props) {
+export function HorizontalLinkListWithIndicator({
+  links = [],
+  layoutId,
+  indicatorProps,
+  ...props
+}: Props) {
   const pathname = usePathname();
   const [mounted, setMounted] = useState(false);
 
@@ -27,12 +33,15 @@ export function HorizontalLinkListWithIndicator({ links = [], indicatorProps, ..
             <NextLink href={href} className='font-semibold'>
               {label}
             </NextLink>
+
             {mounted && isActive && (
-              <Indicator
-                {...indicatorProps}
+              <motion.div
+                transition={{ type: 'spring', stiffness: 500, damping: 30 }}
                 style={{ originY: '0px' }}
+                {...indicatorProps}
+                layoutId={layoutId}
                 className={twMerge(
-                  'left-1/2 -translate-x-1/2 bg-accent-primary w-2 h-2 rounded-full',
+                  'absolute left-1/2 -translate-x-1/2 bg-accent-primary w-2 h-2 rounded-full',
                   indicatorProps?.className
                 )}
               />
