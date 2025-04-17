1cc7bcd4bcbf6f0df501f39b22bf7152f68fde96
SEJ <jink6111@gmail.com>
2025-04-17 15:48:04 +0900

    feat: [NC_publish-HomePage-component-7] published HomePage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
-import { ThemeButton } from '@/shared/components/ThemeButton';
+import { NextLink, ThemeButton } from '@/shared';
 
 export default function HomePage() {
-  return 1;
+  return (
+    <main className='flex flex-col justify-center items-center gap-4 w-full h-dvh'>
+      <ThemeButton iconClassName='size-6 md:size-8' />
+      <h1 className='text-accent-primary text-2xl sm:text-3xl md:text-4xl'>
+        Next.js CRUD reference website.
+      </h1>
+      <h2 className='text-lg sm:text-xl md:text-2xl'>Domain structure.</h2>
+      <NextLink
+        href='https://github.com/SEJ1991/next-crud'
+        className='animate-bounce transition-opacity duration-300 hover:opacity-75 text-sm md:text-base'
+      >
+        Github ↗️
+      </NextLink>
+    </main>
+  );
 }
