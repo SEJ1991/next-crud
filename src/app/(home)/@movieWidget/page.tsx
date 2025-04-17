6ec38e1dd8557ff87af1dd67df2516e0d9ed1d2e
SEJ <jink6111@gmail.com>
2025-04-18 01:14:58 +0900

    feat: [NC-create-MovieWidgetContainer-15] create container and add onClick function
    feat: [NC-create-MovieWidgetContainer-15] write types about movies
    
    feat: [NC-create-MovieWidgetContainer-15] write getAllMovies api
    
    feat: [NC-create-MovieWidgetContainer-15] data fetch to MovieWidget component

--- src/app/(home)/@movieWidget/page.tsx
+++ src/app/(home)/@movieWidget/page.tsx
@@ -1,5 +1,18 @@
-import { MovieWidget } from '@/domains/movie';
+import { MovieWidgetContainer, getAllMovies } from '@/domains/movie';
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 
-export default function MovieWidgetPage() {
-  return <MovieWidget />;
+export const revalidate = 60 * 60;
+
+export default async function MovieWidgetPage() {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ['movies', 'all'],
+    queryFn: () => getAllMovies({ page: 1 }),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <MovieWidgetContainer />
+    </HydrationBoundary>
+  );
 }
