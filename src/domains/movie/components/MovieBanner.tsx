03638f9d0b816763afb27f71eea7fc9856697050
SEJ <jink6111@gmail.com>
2025-04-21 20:43:22 +0900

    feat: [NC_MovieBanner-component-25] write response style
    chore: [NC_MovieBanner-component-25] refine styles

--- src/domains/movie/components/MovieBanner.tsx
+++ src/domains/movie/components/MovieBanner.tsx
@@ -20,6 +20,34 @@ export function MovieBanner({
     release_date,
   },
 }: Props) {
+  const Scores = () => {
+    const trophy = {
+      name: 'trophy',
+      icon: <TrophyIcon />,
+      score: Math.floor(vote_average * 10) / 10,
+    };
+    const star = {
+      name: 'star',
+      icon: <StarIcon />,
+      score: getShortFormatNumber(popularity),
+    };
+    const people = {
+      name: 'people',
+      icon: <PeopleIcon />,
+      score: getShortFormatNumber(vote_count),
+    };
+
+    return (
+      <ul className='flex items-center gap-2'>
+        {[trophy, star, people].map(({ name, icon, score }) => (
+          <li key={name} className='flex justify-center items-center gap-1 text-sm md:text-lg'>
+            {icon}
+            {score}
+          </li>
+        ))}
+      </ul>
+    );
+  };
   return (
     <>
       <section className='relative'>
@@ -52,42 +80,32 @@ export function MovieBanner({
             animate={{ opacity: 1 }}
             exit={{ opacity: 0, position: 'absolute' }}
             transition={{ duration: 0.75, ease: 'easeInOut' }}
-            className='absolute inset-0 flex flex-col gap-2 w-fulltext-white p-4 pt-top-with-header-h sm:gap-4 md:justify-center z-10 md:pt-[10%]'
+            className='absolute inset-0 flex flex-col gap-2 w-full justify-center text-white-primary px-4 py-1 mt-[var(--size-header-height)]'
           >
-            <h2 className='font-semibold text-xl mb-4 xs:text-2xl s:text-5xl md:text-6xl lg:text-7xl'>
+            <h2 className='font-semibold text-lg sm:text-xl md:text-5xl lg:text-7xl'>
               {title}
 
               {adult && (
-                <strong className='text-white bg-red-500 font-semibold rounded-md self-end ml-2 text-sm p-1 s:text-lg s:p-2'>
+                <strong className='bg-red-500 font-semibold rounded-md self-end ml-2 text-sm p-0.5 sm:text-base sm:p-1 md:text-lg lg:text-xl'>
                   19
                 </strong>
               )}
             </h2>
-            <p className='max-w-[80%] line-clamp-2 text-sm s:text-lg sm:line-clamp-3 sm:max-w-[40%] md:text-xl lg:text-2xl '>
+            <p className='w-full line-clamp-1 text-sm sm:text-base sm:line-clamp-2 md:text-lg md:line-clamp-3 lg:max-w-[80%]'>
               {overview}
             </p>
-            <p className='hidden font-semibold md:block'>Release date: {release_date}</p>
-            <ul className='hidden items-center gap-4 md:flex'>
-              <li className='flex justify-center items-center gap-2'>
-                <PeopleIcon className='size-6' />
-                {getShortFormatNumber(vote_count)}
-              </li>
-              <li className='flex justify-center items-center gap-1'>
-                <TrophyIcon className='size-6' />
-                {Math.floor(vote_average * 10) / 10}
-              </li>
-              <li className='flex justify-center items-center gap-1'>
-                <StarIcon className='size-6' />
-                {getShortFormatNumber(popularity)}
-              </li>
-            </ul>
-            <motion.button
-              className='font-semibold bg-gray-800 rounded-md py-2 px-3 self-start'
-              whileTap={{ y: -2 }}
-              // onClick={onClickMore(id)}
-            >
-              More
-            </motion.button>
+            <p className='hidden font-semibold md:block lg:text-xl'>Release date: {release_date}</p>
+            <div className='flex gap-2 md:flex-col'>
+              <Scores />
+              <motion.button
+                className='self-start font-semibold bg-gray-800 rounded-sm text-sm px-1 py-0.5 md:text-base md:px-2 md:py-1.5 md:rounded-md'
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ y: -2 }}
+                // onClick={onClickMore(id)}
+              >
+                More
+              </motion.button>
+            </div>
           </motion.div>
         </AnimatePresence>
       </section>
