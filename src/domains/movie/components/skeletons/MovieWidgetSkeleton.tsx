fd56c4f8b889cb3631d5285b516a1a0601823acd
SEJ <jink6111@gmail.com>
2025-04-23 00:55:02 +0900

    feat: [NC_publish-MovieSkeleton-components-45] published MovieGridListSkeleton component
    chore: [NC_publish-MovieSkeleton-components-45] modify MovieGridList style

--- src/domains/movie/components/skeletons/MovieWidgetSkeleton.tsx
+++ src/domains/movie/components/skeletons/MovieWidgetSkeleton.tsx
@@ -7,7 +7,7 @@ interface Props {
 export function MovieWidgetSkeleton({ onClick }: Props) {
   return (
     <Skeleton
-      className='w-50 aspect-[2/3] rounded-md shadow-primary cursor-pointer'
+      className='w-50 aspect-[2/3] rounded-md shadow-primary cursor-pointer bg-neutral-800'
       whileTap={{ scale: 0.95, zIndex: 1 }}
       whileHover={{ scale: 1.2, zIndex: 1 }}
       transition={{ type: 'spring', stiffness: 300, damping: 20, duration: 0.8 }}
--- src/domains/movie/index.ts
+++ src/domains/movie/index.ts
@@ -1,5 +1,6 @@
 // components
 export * from './components/skeletons/MovieBannerSkeleton';
+export * from './components/skeletons/MovieGridListSkeleton';
 export * from './components/skeletons/MovieWidgetSkeleton';
 export * from './components/MovieBanner';
 export * from './components/MovieGridList';
