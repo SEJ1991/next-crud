8671aace23f6897196134563bbd7121430565394
SEJ <jink6111@gmail.com>
2025-04-22 16:02:42 +0900

    feat: [NC_refactor-MovieHeader-component-33] add home buton in MovieHeader component

--- src/domains/movie/components/MovieHeader.tsx
+++ src/domains/movie/components/MovieHeader.tsx
@@ -1,13 +1,20 @@
 'use client';
 import { MovieNav } from '@/domains/movie';
+import { HomeIcon } from '@/shared';
 import clsx from 'clsx';
 import { useMotionValueEvent, useScroll } from 'framer-motion';
+import { useRouter } from 'next/navigation';
 import { useRef, useState } from 'react';
 
 export function MovieHeader() {
   const ref = useRef<HTMLHeadElement>(null);
+  const router = useRouter();
   const { scrollY } = useScroll();
   const [isAnimated, setIsAnimated] = useState(false);
+
+  const handleClickHome = () => {
+    router.push('/');
+  };
 
   useMotionValueEvent(scrollY, 'change', latest => {
     const headerHeight = ref.current?.offsetHeight ?? 0;
@@ -20,7 +27,7 @@ export function MovieHeader() {
   return (
     <header
       ref={ref}
-      className='fixed top-0 w-full max-w-[var(--size-max-contents-width)] h-[var(--size-header-height)] flex items-center mx-auto px-[var(--size-page-frame-padding-x)] z-30'
+      className='fixed top-0 w-full max-w-[var(--size-max-contents-width)] h-[var(--size-header-height)] flex items-center gap-4 mx-auto px-[var(--size-page-frame-padding-x)] z-30'
     >
       <div
         className={clsx(
@@ -28,6 +35,9 @@ export function MovieHeader() {
           isAnimated ? 'opacity-0' : 'opacity-100'
         )}
       />
+      <button className='z-1' onClick={handleClickHome}>
+        <HomeIcon className='size-6' />
+      </button>
       <MovieNav className='z-1' />
     </header>
   );
