1a500b70a528ba3c9ae34b16ee852800c0a50d65
SEJ <jink6111@gmail.com>
2025-04-28 19:38:50 +0900

    feat: [NC_build-ProductsByCategoryPage-component-83] init build page

--- src/app/products/[category]/page.tsx
+++ src/app/products/[category]/page.tsx
@@ -1,5 +1,58 @@
+import { getAllProducts, getSkip, ProductGridListContainer } from '@/domains/product';
 import { PageFrame } from '@/shared';
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 
-export default function ProductsByCategoryPage() {
-  return <PageFrame>page</PageFrame>;
+export const dynamic = 'force-dynamic';
+interface Props {
+  params: Promise<{ category: string }>;
+  searchParams: Promise<{ [key: string]: string | undefined }>;
+}
+export default async function ProductsByCategoryPage({ params, searchParams }: Props) {
+  const category = (await params).category;
+  const page = getPageParam((await searchParams).page);
+
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ['products', category, page],
+    queryFn: () => getAllProducts({ skip: getSkip(page) }),
+  });
+
+  return (
+    <PageFrame className='flex flex-col gap-8'>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <section className='flex flex-col gap-6'>
+          <h1 className='text-4xl font-semibold'>
+            {category.charAt(0).toUpperCase() + category.slice(1)} products
+          </h1>
+          <ProductGridListContainer page={page} />
+        </section>
+      </HydrationBoundary>
+    </PageFrame>
+  );
+}
+
+function getPageParam(pageParam?: string) {
+  if (!pageParam || isNaN(Number(pageParam))) return 1;
+  return Number(pageParam);
+}
+
+export async function generateMetadata({ params }: Props) {
+  const category = (await params).category;
+  const title = `${category.charAt(0).toUpperCase() + category.slice(1)} products | NEXT-CRUD`;
+  const description = `Browse products by ${category}.`;
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      // url: '',
+    },
+    twitter: {
+      title,
+      description,
+      // card: ''
+    },
+  };
 }
